9587d43e4d07420dc45221b0d001bd82ddb2d240
OniUnderscore <ellysgudger@gmail.com>
2024-05-09 12:24:30 +0100

    Voting! On Articles AND Comments because I cannot read

--- Nc-News/src/api.js
+++ Nc-News/src/api.js
@@ -15,3 +15,11 @@ export function getArticle(article_id) {
 export function getComments(article_id) {
   return api.get(`/articles/${article_id}/comments`);
 }
+
+export function patchComment(comment_id, inc_votes) {
+  return api.patch(`/comments/${comment_id}`, { inc_votes });
+}
+
+export function patchArticle(article_id, inc_votes) {
+  return api.patch(`/articles/${article_id}`, { inc_votes });
+}
--- Nc-News/src/components/Containers/CommentCard.jsx
+++ Nc-News/src/components/Containers/CommentCard.jsx
@@ -1,7 +1,55 @@
-import UpEgg from "../images/Egg_UpEgg.svg?react";
-import DownEgg from "../images/Egg_DownEgg.svg?react";
+import UpEgg from "../images/UpEgg.svg?react";
+import DownEgg from "../images/DownEgg.svg?react";
+import GreyDownEgg from "../images/GreyDownEgg.svg?react";
+import GreyUpEgg from "../images/GreyUpEgg.svg?react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { patchComment } from "../../api";
 
 export function CommentCard({ comment }) {
+  const [vote, setVote] = useState(0);
+  const [votes, setVotes] = useState(comment.votes);
+
+  useEffect(() => {
+    setVotes((currentVotes) => {
+      return votes + vote;
+    });
+  }, [vote]);
+
+  useEffect(() => {
+    patchComment(comment.comment_id, vote);
+  }, [votes]);
+
+  const resetVote = () => {
+    if (vote >= 1) {
+      setVotes((currentVotes) => {
+        return votes - 1;
+      });
+      patchComment(comment.comment_id, -1);
+    } else if (vote <= -1) {
+      setVotes((currentVotes) => {
+        return votes + 1;
+      });
+      patchComment(comment.comment_id, 1);
+    }
+    setVote(0);
+  };
+
+  const upVote = () => {
+    if (vote <= -1) {
+      setVote(2);
+    } else if (vote === 0) {
+      setVote(1);
+    }
+  };
+  const DownVote = () => {
+    if (vote >= 1) {
+      setVote(-2);
+    } else if (vote === 0) {
+      setVote(-1);
+    }
+  };
+
   const created = new Date(comment.created_at);
   return (
     <li className="commentCard">
@@ -11,11 +59,19 @@ export function CommentCard({ comment }) {
       <p className="commentBody">{comment.body}</p>
       <div className="commentVotes">
         <div className="EggButton">
-          <UpEgg />
+          {vote >= 1 ? (
+            <UpEgg onClick={() => resetVote()} />
+          ) : (
+            <GreyUpEgg onClick={() => upVote()} />
+          )}
         </div>
-        <p>{comment.votes}</p>
+        <p>{votes}</p>
         <div className="EggButton">
-          <DownEgg />
+          {vote <= -1 ? (
+            <DownEgg onClick={() => resetVote()} />
+          ) : (
+            <GreyDownEgg onClick={() => DownVote(-1)} />
+          )}
         </div>
       </div>
     </li>
